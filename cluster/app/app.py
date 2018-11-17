53259c1353015c73f4b024d8170a0e1c61b7c121
mpgx5c <mpgx5.c@gmail.com>
2018-11-17 17:08:31 -0300

    novidades

--- cluster/app/app.py
+++ cluster/app/app.py
@@ -9,5 +9,9 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/worker')
+def update():
+    return render_template('worker.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
