7051255e8cc70a7c6bf605410948a4a8cd080c3e
mpgx5c <mpgx5.c@gmail.com>
2018-11-19 17:57:28 -0300

    app client done!

--- src/client/app.py
+++ src/client/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import requests
+import time 
 
 app = Flask(__name__)
 
@@ -9,11 +10,16 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/update')
+def update():
+    data = "Mateus Pintos Garcia"
+    time.sleep(5)
+    return render_template('update.html', data = data )
+
+
 @app.route('/spiner')
 def spiner():
     return render_template('spiner.html')
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
