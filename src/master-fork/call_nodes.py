0c9be021e91f18f13cd4cf7102bbb75f128754be
mpgx5c <mpgx5.c@gmail.com>
2018-11-21 16:17:14 -0300

    news

--- src/master-fork/call_nodes.py
+++
@@ -1,8 +0,0 @@
-import zerorpc
-
-
-def connectNodes():
-    #tem que uma lista dos ips com um laço de repetição
-    Connect = zerorpc.Client()
-    Connect.connect("tcp://127.0.0.1:5432")
-    Connect.start(100)
